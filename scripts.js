babda90c8d2a7b8cf5ef69f3c71bc5171f5ccf86
Sasidharan Senthilkumar <sasidharan192000@gmail.com>
2024-05-06 16:26:12 +0530

    final project

--- scripts.js
+++ scripts.js
@@ -14,3 +14,39 @@ function passwordShow() {
     }
 }
 
+function check() {
+    let passwordValue = document.getElementById("password").value;
+    let lengthCounter = document.querySelector(".count");
+    lengthCounter.textContent =  "Length: " + passwordValue.length;
+
+    if (passwordValue.length >= 5) {
+        document.querySelector(".check1").style.color = "green";
+    } else {
+        document.querySelector("check1").style.color = "red";
+    }
+
+    if (passwordValue.length <= 10) {
+        document.querySelector(".check2").style.color = "green";
+    } else {
+        document.querySelector(".check2").style.color = "red";
+    }
+
+    if (passwordValue.match(/[0-9]/i)) {
+        document.querySelector(".check3").style.color = "green";
+    } else {
+        document.querySelector(".check3").style.color = "red";
+    }
+
+    if (passwordValue.match(/[^A-Za-z0-9-' ']/i)) {
+        document.querySelector(".check4").style.color = "green";
+    } else {
+        document.querySelector(".check4").style.color = "red";
+    }
+
+    if (passwordValue.match(" ")) {
+        document.querySelector(".check5").style.color = "red";
+    } else {
+        document.querySelector(".check5").style.color = "green";
+    }
+}
+
